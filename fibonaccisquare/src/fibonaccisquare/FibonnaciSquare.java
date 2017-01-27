045d3b6e9ed1913d2adf868c7095c7bded2a2bf8
AkshayaNivasini <aksm94@gmail.com>
2017-01-27 15:26:04 +0530

    fibonaccisquare

--- fibonaccisquare/src/fibonaccisquare/FibonnaciSquare.java
+++ fibonaccisquare/src/fibonaccisquare/FibonnaciSquare.java
@@ -1,9 +1,9 @@
 package fibonaccisquare;
 
 public class FibonnaciSquare {
-
-	public static void main(String[] args) {
-
+	public void timetest()
+	{
+		long start=System.currentTimeMillis();
 		long op=0,f0=0,f1=1,f2=0;
 		while(f2<1000000){
 			op=f0+(f1*f1);
@@ -12,9 +12,20 @@ public class FibonnaciSquare {
 			f0=f1;
 			f1=f2;
 		}
+		long stop=System.currentTimeMillis();
+		long totalRuntime=stop-start;
+		System.out.println("Total runtime "+totalRuntime+"ms");
 	}
+	public static void main(String[] args) {
+		FibonnaciSquare fs=new FibonnaciSquare();
+		fs.timetest();
 
 
+
+	}
 }
+
+
+
 
 
